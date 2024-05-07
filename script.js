64f08dc4c499f8116b78ff276160c4065b413953
tomason182 <elotro182@hotmail.com>
2024-05-07 19:28:55 -0300

    structure params object

--- script.js
+++ script.js
@@ -10,21 +10,57 @@ const getWeather = async () => {
   const jsonResponse = await response.json();
 
   const params = {
-    name: "location.name", 
-    region: "location.region", 
-    country: "location.country", 
-    localtime: "location.localtime", 
-    temp_c: "current.temp_c",
-    temp_f: "current.temp_f",
-    humidity: "current.humidity",
-    precip_mm: "current.precip_mm"}
-  
+    name: {
+      dir1: "location",
+      dir2: "name",
+      value: null
+    }, 
+    region: {
+      dir1: "location",
+      dir2: "region",
+      value: null
+    },   
+    country: {
+      dir1: "location", 
+      dir2: "country", 
+      value: null
+    },    
+    localtime: {
+      dir1: "location", 
+      dir2: "localtime",
+      value: null,
+    },
+    temp_c: {
+      dir1: "current", 
+      dir2: "temp_c",
+      value: null
+    },
+    temp_f: {
+      dir1: "current", 
+      dir2: "temp_f",
+      value: null
+    },
+    humidity: {
+      dir1: "current", 
+      dir2: "humidity",
+      value: null
+    },
+    precip_mm: {
+      dir1: "current", 
+      dir2: "precip_mm",
+      value: null
+    }
+  }
 
-  Object.keys(params).forEach((value) => {
-    console.log(params[value]);
+  Object.keys(params).forEach((key) => {
+    const firstParam = params[key].dir1;
+    const secondParam = params[key].dir2;
+    console.log(jsonResponse[firstParam][secondParam]);
+    params[key].value = jsonResponse[firstParam][secondParam];
+
   })
   
-  return jsonResponse;
+  return params;
 }
 
 const getLocation = function getLocationFromInput() {
