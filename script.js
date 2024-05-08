d6a8eb1d38333e1743d27928dd751890c05fac4f
tomason182 <elotro182@hotmail.com>
2024-05-08 11:06:58 -0300

    desconstruct json response
    desconstruc the json responsese into several variables need for the project

--- script.js
+++ script.js
@@ -1,27 +1,101 @@
 const baseUrl =  "https://api.weatherapi.com/v1";
 const apiKey = "ec12d0dee4604907929185302240705";
 
-const fetchWeatherData = async () => {
+// Function to fetch weather data from api
+const fetchWeatherData = async (location) => {
 
-  const location = getLocation();
   const url = `${baseUrl}/current.json?key=${apiKey}&q=${location}&aqi=no`;
-
   try {
     const response = await fetch(url, {mode: "cors"});
     if(!response.ok) {
       throw new Error("Failed to fetch weather data");
     }
-
-    const jsonResponse = await response.json();
-    return jsonResponse;
-
+    return await response.json();
   } catch(error) {
     console.error("Error fetching weather data: ", error);
     throw error;
   }
 }
 
-const getLocationData = async function getLocationDataFromJsonResponse(callback) {
+// Function to extract specific values from JSON response
+
+const getCity = ({location: {name: value}}) => {
+  return value;
+}
+
+const getRegion = ({location: {region: value}}) => {
+  return value;
+}
+
+const getCountry = ({location: {country: value}}) => {
+  return value;
+}
+
+const getLocalTime = ({location: {localtime: value}}) => {
+  return value;
+}
+
+const getTempInCelsius = ({current: {temp_c: value}}) => {
+  return value;
+}
+
+const getTempInFahrenheit = ({current: {temp_f: value}}) => {
+  return value;
+}
+
+const getCondition = ({current: {condition: {text: value}}}) => {
+  return value;
+}
+
+const getConditionIcon = ({current: {condition: {icon: value}}}) => {
+  return value;
+}
+
+const getWindSpeedKph = ({current: {condition: {wind_kph: value}}}) => {
+  return value;
+}
+
+const getHumidity = ({current: {condition: {humidity: value}}}) => {
+  return value;
+}
+
+
+
+
+fetchWeatherData("azul")
+  .then((jsonResponse) => {
+    const cityName = getCity(jsonResponse);
+    const cityRegion = getCity(jsonResponse);
+    const country = getCountry(jsonResponse);
+    const localTime = getLocalTime(jsonResponse);
+    const temp_c = getTempInCelsius(jsonResponse);
+    const temp_f = getTempInFahrenheit(jsonResponse);
+    const conditionText = getCondition(jsonResponse);
+    const conditionIcon = getConditionIcon(jsonResponse);
+    const windSpeedKpm = getWindSpeedKph(jsonResponse);
+    const humidity = getHumidity(jsonResponse);
+
+    console.log(jsonResponse);
+    /* console.log("city Name: ", cityName);
+    console.log("city Region: ", cityRegion);
+    console.log("Country: ", country );
+    console.log("local time: ", localTime);
+    console.log("Temperature in Celsius: ", temp_c);
+    console.log("Temperature in Fahrenheit: ", temp_f);
+    console.log("Condition: ", conditionText);
+    console.log("Icon: ", conditionIcon);
+    console.log("wind speed: ", windSpeedkpm);
+    console.log("Humidity: ", humidity) */
+  })
+  .catch((error) => {
+    console.error("Error: ", error);
+  })
+
+
+
+
+
+/* const getLocationData = async function getLocationDataFromJsonResponse(callback) {
   try {
     const response = await callback();
     const name = response.location.name;
@@ -75,4 +149,4 @@ const finalResult = async () => {
  console.log(result.tempCelcius);
 }
 
-finalResult();
+finalResult(); */
