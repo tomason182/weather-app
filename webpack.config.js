bda067b73a9693c12f96e6000c9e544fc801ac25
tomason182 <elotro182@hotmail.com>
2024-05-09 10:48:28 -0300

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        script: './src/script.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './src',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template: 'src/index.html'
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
