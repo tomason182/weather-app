1102365656a83f981ddc91f14021af3eafde7e9d
tomason182 <elotro182@hotmail.com>
2024-05-09 16:01:53 -0300

    add getSearchValue function
    the function allows to get the input value from the search form

--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@ import { getTodayWeatherData } from "./script";
 
 const today = await getTodayWeatherData();
 
-console.log("Today result: ", today);
+/* console.log("Today result: ", today); */
+
+function getSearchValue() {
+    const searchForm = document.querySelector('#search-container > form');
+
+    searchForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.getElementById('search');        
+        console.log(input.value);
+        input.value = '';
+    })
+}
+
+getSearchValue()
