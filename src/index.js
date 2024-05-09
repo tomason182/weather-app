880cf4d34a59b605f0b6df60858b9dd7cd9e3d99
tomason182 <elotro182@hotmail.com>
2024-05-09 18:56:14 -0300

    add location as title
    insert location information inside title container for todays weather

--- src/index.js
+++ src/index.js
@@ -18,10 +18,23 @@ function getSearchValue() {
 
 const displayWeather = function generateWeatherContainersAndDisplayInfo() {
     const mainContent = document.getElementById('main-content');
+    const location = `${today.city}, ${today.region}, ${today.country} - ${today.localtime}`;
+    console.log(location);
 
     // Create container for display today's weather
     const todayWeatherContainer = document.createElement('div');
     todayWeatherContainer.className = 'today-weather';
+
+    // Inside todays weather container goes to more containers
+    const todayTitleContainer = document.createElement('div');
+    todayTitleContainer.className = 'today-title-container';
+
+    const todayTitle = document.createElement('h2');
+    todayTitle.className = 'today-title';
+    todayTitle.textContent = location;
+
+    todayTitleContainer.appendChild(todayTitle);
+    todayWeatherContainer.appendChild(todayTitleContainer);
 
     // Create container for display today's details
     const todayDetailsContainer = document.createElement('div');
