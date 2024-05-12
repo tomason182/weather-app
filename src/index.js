363d63548aac80025216764f57f0de6693c1f3c8
tomason182 <elotro182@hotmail.com>
2024-05-12 13:37:14 -0300

    create a main container in html template and fix bug

--- src/index.js
+++ src/index.js
@@ -4,35 +4,28 @@ const pageBuilder = {
   // Build 3 main containers.
 
   buildHeader: function() {
+    const mainContainer = document.getElementById('main-container');
     const header = this.createElementWithClassName('div', 'header');
     
     header.appendChild(this.buildLogo());
     header.appendChild(this.buildSearch());
     header.appendChild(this.buildTempSwitch());
 
-    document.body.appendChild(header);
+    mainContainer.appendChild(header);
   },
   buildMainContent: function() {
     
-    const content = this.buildMainContent();
-
-    const sectionOne = this.buildSection1();
-    const sectionTwo = this.buildSection2();
-    const sectionThree = this.buildSection3();
-
-    content.appendChild(sectionOne);
-    content.appendChild(sectionTwo);
-    content.appendChild(sectionThree);
+    // Build main content
 
   },
   buildFooter: function() {
-    this.buildSignature();
+    // build footer
   },
 
   // Header builder
   buildLogo: function() {
     const logoContainer = this.createElementWithClassName('div', 'logo-container');
-    const logoImg = this.createElement('img', 'logo-img');
+    const logoImg = this.createElementWithClassName('img', 'logo-img');
     logoImg.src = '#';
     logoImg.alt = 'Logo';
 
@@ -47,7 +40,7 @@ const pageBuilder = {
     
     searchInput.type = 'text';
     searchInput.name = 'search';
-    searchInput.placeHolder = 'Enter city or zip-code';
+    searchInput.placeholder = 'Enter city or zip-code';
 
     searchForm.appendChild(searchInput);
     searchContainer.appendChild(searchForm);
@@ -56,111 +49,13 @@ const pageBuilder = {
   },
 
   buildTempSwitch: function() {
-    const switchTempContainer = createElementWithClassName('div', 'switch-temp-container');
+    const switchTempContainer = this.createElementWithClassName('div', 'switch-temp-container');
     const switchBtn = this.createElementWithClassName('button', 'switch-btn');
     switchBtn.textContent = 'Switch temp';
 
     switchTempContainer.appendChild(switchBtn);
     return switchTempContainer;
   },
-
-  // Functions that goes inside the main content.
-  buildSection1: function(data) {
-    const currentWeather = document.createElement('div');
-    currentWeather.className = 'section-one';
-
-    // Contenedor del titulo de la seccion 1.
-    const titleContainer = document.createElement('div');
-    titleContainer.className = 'title-container';
-
-    // Content inside title container.
-    const currentLocation = document.createElement('h2');
-    currentLocation.className = 'current-location';
-    currentLocation.textContent = `${data.city}-${data.region}-${data.country}`;
-
-    const localtime = document.createElement('h2');
-    localtime.className = 'localtime';
-    localtime.textContent = data.localtime;
-
-
-    // Append location and local time into title container.
-    titleContainer.appendChild(currentLocation);
-    titleContainer.appendChild(localtime);
-
-    // Subsection One container.
-    const subsectionOne = document.createElement('div');
-    subsectionOne.className = 'subsection-one';
-
-    // Append current temperature and information to subsection one.
-    subsectionOne.appendChild(this.buildCurrentTempContainer(this.today));
-    subsectionOne.appendChild(this.buildCurrentWeatherInfoContainer(this.today, this.todayForecast));
-
-    // Append all three divs to section one container.
-    currentWeather.appendChild(titleContainer);
-    currentWeather.appendChild(subsectionOne);
-
-    return currentWeather;
-  },
-
-  buildSection2: function(data) {
-
-    console.log("section two running");
-    return true;
-  },
-  buildSection3: function() {
-    console.log("section three running");
-    return true;
-  },
-
-  buildCurrentTempContainer: function(data) {
-    const temp_c = data.temp_c;
-    const temp_f = data.temp_f;
-    const currentTempContainer = document.createElement('div');
-    currentTempContainer.className = 'temperature-container';
-
-    const currentTemp = document.createElement('h2');
-    currentTemp.className = 'current-temperature';
-    currentTemp.textContent = `${temp_c}°C`;
-
-    const currentStatus = document.createElement('h4');
-    currentStatus.className = 'current-status-text';
-    currentStatus.textContent = data.condition_text;
-
-    const currentStatusIcon = document.createElement('img');
-    currentStatusIcon.className = 'current-status-img';
-    currentStatusIcon.src = data.condition_icon;
-
-    currentTempContainer.appendChild(currentTemp);
-    currentTempContainer.appendChild(currentStatus);
-    currentTempContainer.appendChild(currentStatusIcon);
-
-    return currentTempContainer;
-  },
-
-  buildCurrentWeatherInfoContainer: function(firstData, secondData) {
-    // Contenedor informacion actual del clima.
-    const currentWeatherInfoContainer = document.createElement('div');
-    currentWeatherInfoContainer.className = 'current-weather-info-container';
-
-    const windSpeed = document.createElement('h4');
-    windSpeed.className = 'current-weather-info';
-    windSpeed.textContent = `Wind: ${firstData.wind_kph} km`;
-
-    const humidity = document.createElement('h4');
-    humidity.className= 'current-weather-info';
-    humidity.textContent = `Humidity: ${firstData.humidity}%`;
-
-    const rain_prob = document.createElement('h4');
-    rain_prob.className = 'current-weather-info';
-    rain_prob.textContent = `Rain probability: ${secondData.rain_prob}`;
-
-
-    currentWeatherInfoContainer.appendChild(windSpeed);
-    currentWeatherInfoContainer.appendChild(humidity);
-    currentWeatherInfoContainer.appendChild(rain_prob);
-
-    return currentWeatherInfoContainer;
-  }
 
   createElementWithClassName: function(type, className) {
     const element = document.createElement(type);
@@ -170,4 +65,3 @@ const pageBuilder = {
 }
 
 pageBuilder.buildHeader();
-pageBuilder.buildMainContent();
