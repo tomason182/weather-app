144f5d84a617602dfdaefb1b00a9ea9f11802b25
tomason182 <elotro182@hotmail.com>
2024-05-12 20:20:44 -0300

    add style to first section title

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     entry: {
         index: './src/index.js',
         script: './src/script.js',
+        content: './src/content.js',
     },
     devtool: 'inline-source-map',
     devServer: {
