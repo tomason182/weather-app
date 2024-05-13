c1d9546cbbb6c29da6c1717f77598a730169e31a
tomason182 <elotro182@hotmail.com>
2024-05-13 15:38:13 -0300

    add feelslike to container

--- src/content.js
+++ src/content.js
@@ -18,20 +18,26 @@ const addContent = {
         const celsiusBtn = document.querySelector('.celsius-container');
         const fahrenheitBtn = document.querySelector('.fahrenheit-container');
         const tempContainer = document.querySelector('.weather-temp-container');
-        const weatherTempElement = createElementWithClassName('h1', 'temperature-value');        
+        const feelLikeElement = document.querySelector('.first-section-container-two > .weather-detail.Feel-like > .condition-value');
+        console.log(feelLikeElement);
+        const weatherTempElement = createElementWithClassName('h1', 'temperature-value');
 
         const temp_c = await this.getTempInCelsius(data);
         const temp_f = await this.getTempInFahrenheit(data);
+        const feelslike_c = await this.getFeelsLikeinCelsius(data);
+        const feelslike_f = await this.getFeelsLikeInFahrenheit(data);
     
         celsiusBtn.classList.add('active');
         weatherTempElement.textContent = `${temp_c} °C`;
+        feelLikeElement.textContent = `${feelslike_c} °C`;
         
         const handleCelsiusClick = () => {
             if(!celsiusBtn.classList.contains('active')) {
                 celsiusBtn.classList.add('active');
                 fahrenheitBtn.classList.remove('active');
-                console.log(temp_c);
                 weatherTempElement.textContent = `${temp_c} °C`;
+                feelLikeElement.textContent = `${feelslike_c} °C`;
+
                 return weatherTempElement;
             }
         };
@@ -40,8 +46,8 @@ const addContent = {
             if(!fahrenheitBtn.classList.contains('active')) {
                 fahrenheitBtn.classList.add('active');
                 celsiusBtn.classList.remove('active');
-                console.log(temp_f);
                 weatherTempElement.textContent = `${temp_f} °F`;
+                feelLikeElement.textContent = `${feelslike_f} °F`;
                 return weatherTempElement;
             }
         };
@@ -99,6 +105,16 @@ const addContent = {
     getWeatherCondition: async function(data) {
         const obj = await data;
         return obj.condition_text;
+    },
+
+    getFeelsLikeinCelsius: async function(data) {
+        const obj = await data;
+        return obj.feelslike_c;
+    },
+
+    getFeelsLikeInFahrenheit: async function(data) {
+        const obj = await data;
+        return obj.feelslike_f;
     }
 
 }
