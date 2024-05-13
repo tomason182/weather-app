91f60355bf3aacd11092c07e9ea6c538876b488c
tomason182 <elotro182@hotmail.com>
2024-05-13 15:11:41 -0300

    add second class name to createElement function

--- src/create-element.js
+++ src/create-element.js
@@ -1,8 +1,11 @@
-const createElementWithClassName = function (elementType, className) {
+const createElementWithClassName = function (elementType, className1, className2) {
     const element = document.createElement(elementType);
     
-    if (className) {
-      element.classList.add(className); // Can add more than one class name separate with space.
+    if (className1) {
+      element.classList.add(className1); // Can add more than one class name separate with space.
+    }
+    if (className2) {
+      element.classList.add(className2);
     }
     
     return element
