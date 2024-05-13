5cd9823b0362dd05397bb390f647f797aafac5cb
tomason182 <elotro182@hotmail.com>
2024-05-13 14:05:08 -0300

    fix handle switch btn

--- src/content.js
+++ src/content.js
@@ -15,9 +15,45 @@ const addContent = {
     },
 
     addTemp: async function(data) {
-        const weatherTempContainer = document.querySelector('.weather-temp-container');
-        const switchUnitBtn = document.querySelector('.switch-btn');
-        console.log(switchUnitBtn);
+        const celsiusBtn = document.querySelector('.celsius-container');
+        const fahrenheitBtn = document.querySelector('.fahrenheit-container');
+        const tempContainer = document.querySelector('.weather-temp-container');
+        const weatherTempElement = createElementWithClassName('h1', 'temperature-value');
+
+        
+
+        const temp_c = await this.getTempInCelsius(data);
+        const temp_f = await this.getTempInFahrenheit(data);
+    
+        celsiusBtn.classList.add('active');
+        weatherTempElement.textContent = temp_c;
+        
+        const handleCelsiusClick = () => {
+            if(!celsiusBtn.classList.contains('active')) {
+                celsiusBtn.classList.add('active');
+                fahrenheitBtn.classList.remove('active');
+                console.log(temp_c);
+                weatherTempElement.textContent = temp_c;
+                return weatherTempElement;
+            }
+        };
+    
+        const handleFahrenheitClick = () => {
+            if(!fahrenheitBtn.classList.contains('active')) {
+                fahrenheitBtn.classList.add('active');
+                celsiusBtn.classList.remove('active');
+                console.log(temp_f);
+                weatherTempElement.textContent = temp_f;
+                return weatherTempElement;
+            }
+        };
+    
+        celsiusBtn.addEventListener('click', handleCelsiusClick);
+        fahrenheitBtn.addEventListener('click', handleFahrenheitClick);
+
+        tempContainer.appendChild(weatherTempElement);
+    
+        return data.temp_c;
     },
 
     getLocation: async function(data) {
@@ -35,6 +71,11 @@ const addContent = {
         return obj.temp_c;
     },
 
+    getTempInFahrenheit: async function(data) {
+        const obj = await data;
+        return obj.temp_f;
+    }
 }
+
 
 export {addContent};
